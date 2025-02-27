4855ceb0b48188dc3bdac2116fdf0914b1c1cfcd
VarunDRS <varuns.cs21@bmsce.ac.in>
2025-02-27 18:02:24 +0530

    Varuns commit

---
+++ backend/slack-hrbp/src/main/java/com/cars24/slack_hrbp/data/entity/Attendance.java
@@ -0,0 +1,66 @@
+package com.cars24.slack_hrbp.data.entity;
+
+import org.springframework.data.annotation.Id;
+import org.springframework.data.mongodb.core.mapping.Document;
+
+@Document(collection = "Attendance")
+public class Attendance {
+
+    @Id
+    private String id;
+    private String userid;
+    private String username;
+    private String date;
+    private String type;
+    private String reason;
+
+    // Getters and Setters
+    public String getId() {
+        return id;
+    }
+
+    public void setId(String id) {
+        this.id = id;
+    }
+
+    public String getUserid() {
+        return userid;
+    }
+
+    public void setUserid(String userid) {
+        this.userid = userid;
+    }
+
+    public String getUsername() {
+        return username;
+    }
+
+    public void setUsername(String username) {
+        this.username = username;
+    }
+
+    public String getDate() {
+        return date;
+    }
+
+    public void setDate(String date) {
+        this.date = date;
+    }
+
+    public String getType() {
+        return type;
+    }
+
+    public void setType(String type) {
+        this.type = type;
+    }
+
+    public String getReason() {
+        return reason;
+    }
+
+    public void setReason(String reason) {
+        this.reason = reason;
+    }
+
+}
