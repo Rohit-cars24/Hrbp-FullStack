baab3ec4640583a76696153e9f8e7a9a62398a37
Rohit-cars24 <r.rohit@cars24.com>
2025-02-27 11:52:21 +0530

    seting up

---
+++ backend/slack-hrbp/src/main/java/com/cars24/slack_hrbp/SlackHrbpApplication.java
@@ -0,0 +1,13 @@
+package com.cars24.slack_hrbp;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SlackHrbpApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SlackHrbpApplication.class, args);
+	}
+
+}
